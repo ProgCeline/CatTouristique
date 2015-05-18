c6c7610f2c673ec49897f2eb89b91c305b57bae5
KingamaJ <jordy.kingama@gmail.com>
2015-05-18 17:00:41 +0200

    classe etablissement

---
+++ app/src/main/java/fr/imerir/cattouristique/Etablissement.java
@@ -0,0 +1,92 @@
+package fr.imerir.cattouristique;
+
+/**
+ * Created by rcdsm on 18/05/15.
+ */
+public class Etablissement {
+    public  String name;
+    public  String type;
+    public  String adresse;
+    public  String photo_link;
+
+    public  float latitude;
+    public  float longitude;
+
+    public Etablissement(String _name, String _type, String _adresse, String _photo_link, float _latitude, float _longitude) {
+        this.name = _name;
+        this.type = _type;
+        this.adresse = _adresse;
+        this.photo_link = _photo_link;
+        this.latitude = _latitude;
+        this.longitude = _longitude;
+    }
+
+    public Etablissement() {
+        this.name = "";
+        this.type = "";
+        this.adresse = "";
+        this.photo_link = "";
+        this.latitude = 0.0f;
+        this.longitude = 0.0f;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getPhoto_link() {
+        return photo_link;
+    }
+
+    public void setPhoto_link(String photo_link) {
+        this.photo_link = photo_link;
+    }
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+
+    @Override
+    public String toString() {
+        return "Etablissement{" +
+                "name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", adresse='" + adresse + '\'' +
+                ", photo_link='" + photo_link + '\'' +
+                ", latitude=" + latitude +
+                ", longitude=" + longitude +
+                '}';
+    }
+}
